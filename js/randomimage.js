ea7651d6dc1197281edcb871f8b035c0f391e3b9
Mathias Villemsoo <mathias.villemsoo@tktk.ee>
2024-11-07 14:03:04 +0200

    Javascript

---
+++ js/randomimage.js
@@ -0,0 +1,13 @@
+const myButton = document.querySelector('button');
+const image = document.querySelector('img');
+const images = ["pic1.jpeg", "pic2.jpeg", "pic3.jpeg", "pic4.jpeg"];
+
+
+myButton.addEventListener('click', changeImage);
+
+function changeColor() {
+    let randomIndex = Math.floor(Math.random() * images.length);
+    console.log(randomIndex);
+    image.src = "images/" + images[randomIndex];
+
+}
