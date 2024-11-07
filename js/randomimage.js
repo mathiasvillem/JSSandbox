963b419718d6b279a135935bfef7f99aef605fe0
Mathias Villemsoo <mathias.villemsoo@tktk.ee>
2024-11-07 14:29:06 +0200

    fix function name

--- js/randomimage.js
+++ js/randomimage.js
@@ -5,7 +5,7 @@ const images = ["pic1.jpeg", "pic2.jpeg", "pic3.jpeg", "pic4.jpeg"];
 
 myButton.addEventListener('click', changeImage);
 
-function changeColor() {
+function changeImage() {
     let randomIndex = Math.floor(Math.random() * images.length);
     console.log(randomIndex);
     image.src = "images/" + images[randomIndex];
